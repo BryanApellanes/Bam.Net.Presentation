75ce8fea875e4012f2d98f6dce2e321452835960
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-11-21 07:18:12 -0800

    move tests outs

--- Tests/bam.net.presentation.core.tests/SemanticAssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using Bam.Net;
-
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.742579000")]
-[assembly: AssemblyCommit("60914af")]
-[assembly: AssemblySemanticVersion("1.0.5-test+60914af")]
